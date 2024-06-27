cec2ad097d0fd25baa054252f0486c75c0b2e17c
akaMiWP <bangkokappdevelopment@gmail.com>
2024-06-27 16:07:23 +0700

    Added a loading indicator

--- react-effect-hook/src/components/Users.tsx
+++ react-effect-hook/src/components/Users.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 import React, { useEffect, useState } from "react";
 
 interface User {
@@ -9,15 +9,24 @@ interface User {
 const Users = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState<AxiosError>(null);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
-      .catch((err) => setError(err));
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err);
+        setLoading(false);
+      });
 
     return () => controller.abort();
   }, []);
@@ -25,6 +34,7 @@ const Users = () => {
   return (
     <div className="mb-3">
       {error && <p className="text-danger">{error.message}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((value) => (
           <li key={value.id}>{value.name}</li>
