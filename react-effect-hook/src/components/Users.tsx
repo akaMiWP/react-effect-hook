c316a6fd77ee843d2921286a847d90903b1a772d
akaMiWP <bangkokappdevelopment@gmail.com>
2024-06-28 20:58:50 +0700

    Created custom data hook for fetching data

--- react-effect-hook/src/components/Users.tsx
+++ react-effect-hook/src/components/Users.tsx
@@ -1,29 +1,10 @@
 import { AxiosError, CanceledError } from "../services/api-client";
 import { useEffect, useState } from "react";
 import userService, { User } from "../services/user-service";
+import useUsers from "../hooks/useUsers";
 
 const Users = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState<AxiosError>(null);
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err);
-        setLoading(false);
-      });
-
-    return cancel;
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const addUser = () => {
     const originalUsers = [...users];
